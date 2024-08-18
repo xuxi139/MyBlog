fff3de6fd0459e660168c5090f0d95bf9f8510e8
xuxi139 <161686109+xuxi139@users.noreply.github.com>
2024-08-18 19:45:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle mobile menu
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+  const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
+  });
+});
